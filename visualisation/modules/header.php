07edf3890d764194f1bd98343239bff70d0803b9
brecht <brecht.baeten@gmail.com>
2014-10-07 11:57:16 +0200

    added link to alarms page in header and alarm icons

--- visualisation/modules/header.php
+++ visualisation/modules/header.php
@@ -4,6 +4,7 @@ echo "
 			<a href='index.php'><img src='icons/ws/control_building_empty.png'></a>
 			<a href='index.php?page=modules/measurements'><img src='icons/ws/measure_power_meter.png'></a>
 			<a href='index.php?page=modules/settings'><img src='icons/ws/edit_settings.png'></a>
+			<a href='index.php?page=modules/alarms'><img src='icons/ws/control_alarm.png'></a>
 		</nav>
 ";
 ?>
