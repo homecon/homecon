eb177be70a0e3e1e3c34a83ce64dce508eadfc34
brecht <brecht.baeten@gmail.com>
2014-10-07 12:45:19 +0200

    actionlist structure updated

--- visualisation/modules/alarms.php
+++ visualisation/modules/alarms.php
@@ -7,7 +7,7 @@ begin_article($page_class);
 		begin_group(1);
 		
 	echo "
-				<form action='index.php?page=modules/alarms' method='post' name='define' class='define'>";
+			<form action='index.php?page=modules/alarms' method='post' name='define' class='define'>";
 	$result = mysql_query("SELECT * FROM alarm_actions");
 	$count = 0;
 	while($row = mysql_fetch_array($result)){
@@ -33,10 +33,9 @@ begin_article($page_class);
 		$item5 = $row['item5'];
 		$action5 = $row['action5'];
 		
-		
-		
 		$id_id = "id".$id;
 		$name_id = "name".$id;
+		$sectionid_id = "sectionid".$id;
 		
 		$delay1_id = "delay1".$id;
 		$item1_id = "item1".$id;
@@ -54,23 +53,43 @@ begin_article($page_class);
 		$item5_id = "item5".$id;
 		$action5_id = "action5".$id;
 		
-		$readonly = "";
-		if($count<=35){
-			$readonly = "readonly";
-		}
 		echo "
+				<div>
+					<a class='delete' href='index.php?web=$web&page=modules/alarms&delete_action=$id'><img src=icons/ws/control_x.png></a>
+					<input type='text' name='$id_id'          id='$id_id'        value='$id' disabled placeholder='id'>
+					<input type='text' name='$name_id'        id='$name_id'      value='$id' disabled placeholder='name'>
+					<input type='text' name='$sectionid_id'   id='$sectionid_id' value='$id' disabled placeholder='name'>
 					<div>
-						<input type='text' name='$id_id'          id='$id_id'        value='$id' disabled placeholder='id'>
-						<input type='text' name='$item_id'        id='$item_id'      value='$item' $readonly placeholder='item'>
-						<input type='text' name='$name_id'        id='$name_id'      value='$name' placeholder='name'>
-						<input type='text' name='$quantity_id'    id='$quantity_id'  value='$quantity' placeholder='quantity'>
-						<input type='text' name='$unit_id'        id='$unit_id'      value='$unit' placeholder='unit'>
-						<input type='text' name='$description_id' id='$description_id' value='$description' placeholder='description'>
-					</div>";
+						<input type='number' name='$delay1_id'   id='$delay1_id'  value='$delay1'  placeholder='delay 1'>
+						<input type='text'   name='$item1_id'    id='$item1_id'   value='$item1'   placeholder='item 1'>
+						<input type='text'   name='$action1_id'  id='$action1_id' value='$action1' placeholder='action 1'>
+					</div>
+					<div>
+						<input type='number' name='$delay2_id'   id='$delay2_id'  value='$delay2'  placeholder='delay 2'>
+						<input type='text'   name='$item2_id'    id='$item2_id'   value='$item2'   placeholder='item 2'>
+						<input type='text'   name='$action2_id'  id='$action2_id' value='$action2' placeholder='action 2'>
+					</div>
+					<div>
+						<input type='number' name='$delay3_id'   id='$delay3_id'  value='$delay3'  placeholder='delay 3'>
+						<input type='text'   name='$item3_id'    id='$item3_id'   value='$item3'   placeholder='item 3'>
+						<input type='text'   name='$action3_id'  id='$action3_id' value='$action3' placeholder='action 3'>
+					</div>
+					<div>
+						<input type='number' name='$delay4_id'   id='$delay4_id'  value='$delay4'  placeholder='delay 4'>
+						<input type='text'   name='$item4_id'    id='$item4_id'   value='$item4'   placeholder='item 4'>
+						<input type='text'   name='$action4_id'  id='$action4_id' value='$action4' placeholder='action 4'>
+					</div>
+					<div>
+						<input type='number' name='$delay5_id'   id='$delay5_id'  value='$delay5'  placeholder='delay 5'>
+						<input type='text'   name='$item5_id'    id='$item5_id'   value='$item5'   placeholder='item 5'>
+						<input type='text'   name='$action5_id'  id='$action5_id' value='$action5' placeholder='action 5'>
+					</div>
+				</div>";
 	}
 	echo "
-					<button type='submit' name='define'>Submit</button>
-				</form>";
+				<a class='add' href='#' data-role='button'>Add action</a>
+				<button type='submit' name='define'>Submit</button>
+			</form>";
 				
 		end_group();
 	end_collapsible();		
