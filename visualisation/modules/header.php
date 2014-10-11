a400dac7a4c433d2dfecb508a2cd3a5c39cce9ae
brecht <brecht.baeten@gmail.com>
2014-10-11 16:13:59 +0200

    indentation changed

--- visualisation/modules/header.php
+++ visualisation/modules/header.php
@@ -1,10 +1,10 @@
 <?php
 echo "
-		<nav>
-			<a href='index.php'><img src='icons/ws/control_building_empty.png'></a>
-			<a href='index.php?page=modules/measurements'><img src='icons/ws/measure_power_meter.png'></a>
-			<a href='index.php?page=modules/settings'><img src='icons/ws/edit_settings.png'></a>
-			<a href='index.php?page=modules/alarms'><img src='icons/ws/control_alarm.png'></a>
-		</nav>
+				<nav>
+					<a href='index.php'><img src='icons/ws/control_building_empty.png'></a>
+					<a href='index.php?page=modules/measurements'><img src='icons/ws/measure_power_meter.png'></a>
+					<a href='index.php?page=modules/settings'><img src='icons/ws/edit_settings.png'></a>
+					<a href='index.php?page=modules/alarms'><img src='icons/ws/control_alarm.png'></a>
+				</nav>
 ";
 ?>
