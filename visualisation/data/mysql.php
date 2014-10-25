f38ea7285163096475d339f54730f7e0b02db2be
brecht <brecht.baeten@gmail.com>
2014-10-26 00:35:04 +0200

    changed default knxcontrol mysql password to admin

--- visualisation/data/mysql.php
+++ visualisation/data/mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!mysql_connect("localhost","knxcontrol","password")) {
+if (!mysql_connect("localhost","knxcontrol","admin")) {
   die('Could not connect: ' . mysql_error());
 }
 mysql_select_db("knxcontrol");
