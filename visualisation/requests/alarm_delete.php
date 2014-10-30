d7400a5be1647243756ba893cb506ea135077b76
brecht <brecht.baeten@gmail.com>
2014-10-30 17:38:42 +0100

    added user authentication

--- visualisation/requests/alarm_delete.php
+++ visualisation/requests/alarm_delete.php
@@ -2,10 +2,11 @@
 
 	include('../data/mysql.php');
 	
-	$id = $_POST['id'];
-	
-	// add alarm to the mysql database
-	$query = "DELETE FROM alarms WHERE id=$id";
-	$result = mysql_query($query) or die('Error: ' . mysql_error());
-
+	if($_SESSION['userid']>0){
+		$id = $_POST['id'];
+		
+		// add alarm to the mysql database
+		$query = "DELETE FROM alarms WHERE id=$id";
+		$result = mysql_query($query) or die('Error: ' . mysql_error());
+	}
 ?>
--- visualisation/requests/alarm_set.php
+++ visualisation/requests/alarm_set.php
@@ -2,40 +2,39 @@
 
 	include('../data/mysql.php');
 
-	$id = $_POST['id'];
-	$column = $_POST['column'];
-	$value = $_POST['value'];
+	if($_SESSION['userid']>0){
 	
-	
-	
-	// convert value array to comma seperated list
-	if(is_array($value)){
-		$value = implode(',',$value);
-	}
-	
-	// used for debugging
-	echo "$id, $column, $value";
-	
-	
-	if(strcmp($column,'time')==0){
-		// if column is time
-		$list = explode(':',$value);
-
-		$hour = intval($list[0]);
-		$minute = intval($list[1]);
+		$id = $_POST['id'];
+		$column = $_POST['column'];
+		$value = $_POST['value'];
 		
-		$query = "UPDATE alarms SET hour=$hour, minute=$minute WHERE id=$id";
-		$result = mysql_query($query) or die('Error: ' . mysql_error());
-	}
-	elseif(strcmp($column,'item')==0 || strcmp($column,'action')==0){
-		// if column is item or action
-		$query = "UPDATE alarms SET $column='$value' WHERE id=$id";
-		$result = mysql_query($query) or die('Error: ' . mysql_error());
-	}
-	else{
-		// else column is a day
-		$query = "UPDATE alarms SET $column=$value WHERE id=$id";
-		$result = mysql_query($query) or die('Error: ' . mysql_error());
-	}
+		// convert value array to comma seperated list
+		if(is_array($value)){
+			$value = implode(',',$value);
+		}
+		
+		// used for debugging
+		echo "$id, $column, $value";
+			
+		if(strcmp($column,'time')==0){
+			// if column is time
+			$list = explode(':',$value);
 
+			$hour = intval($list[0]);
+			$minute = intval($list[1]);
+			
+			$query = "UPDATE alarms SET hour=$hour, minute=$minute WHERE id=$id";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		elseif(strcmp($column,'item')==0 || strcmp($column,'action')==0){
+			// if column is item or action
+			$query = "UPDATE alarms SET $column='$value' WHERE id=$id";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+		else{
+			// else column is a day
+			$query = "UPDATE alarms SET $column=$value WHERE id=$id";
+			$result = mysql_query($query) or die('Error: ' . mysql_error());
+		}
+	}
 ?>
