69c039f31d45fb23643dc09e089aef9c978ba426
brecht <brecht.baeten@gmail.com>
2014-11-01 11:27:32 +0100

    added bars link to open menu on small screens

--- visualisation/js/menu.js
+++ visualisation/js/menu.js
@@ -1,9 +1,17 @@
 
-// menu swipe
-$(document).on("pageinit", function() {
-    $(document).on( "swiperight", function(){
+$(document).on('pageinit', function() {
+	// menu swipe
+    $(document).on('swiperight', function(){
 		if($(window).width()<800){
 			$.mobile.activePage.find('#menu').panel("open");			
 		}
     });
+	
+	// menu bars
+	$(document).on('click','#menu_button', function(){
+		if($(window).width()<800){
+			$.mobile.activePage.find('#menu').panel("toggle");			
+		}
+    });
 });
+
