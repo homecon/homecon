d90c77d2ebe17b013a47b4691f6d9c033631a197
brecht <brecht.baeten@gmail.com>
2014-11-06 09:29:06 +0100

    removed oninit

--- visualisation/js/menu.js
+++ visualisation/js/menu.js
@@ -1,17 +1,16 @@
 
-$(document).on('pageinit', function() {
-	// menu swipe
-    $(document).on('swiperight', function(){
-		if($(window).width()<800){
-			$.mobile.activePage.find('#menu').panel("open");			
-		}
-    });
-	
-	// menu bars
-	$(document).on('click','#menu_button', function(){
-		if($(window).width()<800){
-			$.mobile.activePage.find('#menu').panel("toggle");			
-		}
-    });
+// menu swipe
+$(document).on('swiperight', function(){
+	if($(window).width()<800){
+		$.mobile.activePage.find('#menu').panel("open");			
+	}
 });
+
+// menu bars
+$(document).on('click','#menu_button', function(){
+	if($(window).width()<800){
+		$.mobile.activePage.find('#menu').panel("toggle");			
+	}
+});
+
 
