40eb879f7444cec932294847ae38d96216cd2aac
brecht <brecht.baeten@gmail.com>
2014-11-06 08:09:48 +0100

    updated bar chart macro

--- visualisation/js/charts.js
+++ visualisation/js/charts.js
@@ -15,68 +15,6 @@ $(document).on('pagebeforecreate',function(){
 		var options = {
 			chart: {
 				renderTo: id,
-				type: 'line'
-			},
-			title: {
-				text: title_str
-			},
-			xAxis: {
-				type: 'datetime',
-				range: 2 * 24 * 3600 * 1000
-			},
-			tooltip: {
-				xDateFormat: '%Y-%m-%d %H:%M',
-				valueDecimals: 1,
-				shared: true
-			},
-			rangeSelector : {
-				enabled: false
-			},
-			series: []
-		}
-		
-		// tell highcharts to convert utc time to local time
-		Highcharts.setOptions({
-			global: {
-				useUTC: false
-			}
-		});
-		
-		chart[j] = new Highcharts.StockChart(options);
-
-		console.time("total");
-		
-		// Load data asynchronously using jQuery. On success, add the data to the options and initiate the chart.
-		jQuery.each(signals_str.split(/,/), function(i, signal_id) {
-
-			$.post('requests/measurements_get.php?signal='+signal_id+'&scale=quarter',function(series){
-			
-				var series = JSON.parse(series);
-				
-				chart[j].addSeries({name: series.name ,data: series.data});
-				chart[j].yAxis[0].setTitle({text:series.unit});
-
-				
-			});
-		
-		});
-		console.timeEnd("total");
-	});
-	
-	
-	var barchart = [];
-	//cycle through all bar_chart_placeholder
-	$( ".bar_chart_placeholder" ).each(function(j){
-	
-		var id = $(this).attr('id');
-		var title_str = $(this).attr('data-title');
-		var signals_str = $(this).attr('data-signals');
-		
-		// set some options
-		var options = {
-			chart: {
-				renderTo: id,
-				type: 'column'
 			},
 			title: {
 				text: title_str
@@ -86,6 +24,7 @@ $(document).on('pagebeforecreate',function(){
 			},
 			tooltip: {
 				xDateFormat: '%Y-%m-%d',
+				valueDecimals: 1,
 				shared: true
 			},
 			rangeSelector : {
@@ -93,7 +32,7 @@ $(document).on('pagebeforecreate',function(){
 			},
 			series: []
 		}
-		
+
 		// tell highcharts to convert utc time to local time
 		Highcharts.setOptions({
 			global: {
@@ -101,22 +40,31 @@ $(document).on('pagebeforecreate',function(){
 			}
 		});
 		
-		barchart[j] = new Highcharts.StockChart(options);
-
-
+		// change options depending on the chart type
+		if($(this).attr('data-chart')=='bar'){
+			options.chart.type = 'bar';
+			chart[j] = new Highcharts.Chart(options);
+		}
+		else{
+			options.chart.type = 'line';
+			options.xAxsis.range = 2 * 24 * 3600 * 1000;
+			options.tooltip.xDateFormat='%Y-%m-%d %H:%M';
+			chart[j] = new Highcharts.StockChart(options);
+		}
+		
+		
 		// Load data asynchronously using jQuery. On success, add the data to the options and initiate the chart.
 		jQuery.each(signals_str.split(/,/), function(i, signal_id) {
 
-			$.post('requests/measurements_get.php?signal='+signal_id+'&scale=week',function(series){
+			$.post('requests/measurements_get.php?signal='+signal_id+'&scale=quarter',function(series){
 			
 				var series = JSON.parse(series);
 				
-				barchart[j].addSeries({name: series.name ,data: series.data});
-				barchart[j].yAxis[0].setTitle({text:series.unit});
+				// update the chart
+				chart[j].addSeries({name: series.name ,data: series.data});
+				chart[j].yAxis[0].setTitle({text:series.unit});
 
-				
 			});
-		
 		});
 	});
 });
