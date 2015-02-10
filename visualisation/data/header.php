649d5fadd46148e16eea249a3659438c20b0245f
brecht <brecht.baeten@gmail.com>
2015-02-10 14:27:47 +0100

    extra buttons in header are hidden on small screens

--- visualisation/data/header.php
+++ visualisation/data/header.php
@@ -2,9 +2,9 @@
 		<div id='header' data-role='header' data-position='fixed' data-tap-toggle='false' data-theme='b'>
 			<nav>
 				<a href='#home'><img src='icons/ws/control_building_empty.png'></a>
-				<a href='#alarms'><img src='icons/ws/control_alarm.png'></a>
-				<a href='#measurements'><img src='icons/ws/measure_power_meter.png'></a>
-				<a href='#settings'><img src='icons/ws/edit_settings.png'></a>
+				<a class='hide' href='#alarms'><img src='icons/ws/control_alarm.png'></a>
+				<a class='hide' href='#measurements'><img src='icons/ws/measure_power_meter.png'></a>
+				<a class='hide' href='#settings'><img src='icons/ws/edit_settings.png'></a>
 				<a href='#' id='menu_button'><img src='icons/ws/control_bars.png'></a>
 			</nav>
 		</div>
