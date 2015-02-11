e7b48f4899f2b6412155d6d9ad78e5d37e2a7df7
brecht <brecht.baeten@gmail.com>
2015-02-11 08:22:49 +0100

    tested first pagebuilder rendering

--- visualisation/js/pagebuilder.js
+++ visualisation/js/pagebuilder.js
@@ -1,28 +1,33 @@
 
 
 pagebuilder = {
-	model: {
-		home: {
-			name: 'Home',
-			section: [
-				{
-					type: 'clock'
-				},
-				{
-					type: 'weather_current'
-				},
-				{
-					type: 'weather_forecast'
-				},
-			]
-		},
-		section: [];
+	home: {
+		name: 'Home',
+		section: [
+			{
+				type: ''
+			},
+			{
+				type: ''
+			},
+			{
+				type: ''
+			},
+		]
 	},
-	add_menu_section: function(){
-		model.section.push({
+	section: [],
+	publish: function(){
+		// create pages.php
+	},
+	add_section: function(){
+		pagebuilder.section.push({
 			id: '',
 			name: '',
 			page: [],
+			update: function(id,name){
+				this.id = id;
+				this.name = name;
+			},
 			add_page: function(){
 				this.page.push({
 					id: '',
@@ -30,20 +35,62 @@ pagebuilder = {
 					img: '',
 					temperature_item:'',
 					section: [],
+					update: function(id,name,img,temperature_item){
+						this.id = id;
+						this.name = name;
+						this.img = img;
+						this.temperature_item = temperature_item;
+					},
 					add_section: function(){
 						this.section.push({
 							name: '',
-							group: [],
-							add_control: function(group,widget,options){
-							
+							type: '',
+							widget: [],
+							add_widget: function(widget,options){
+								this.widget.push({
+									
+								});
 							},
-							del_control: function(group,widget){
+							del: function(){
+								delete this;
 							
 							},
 						});
 					},
 					del: function(){
 						delete this;
+					},
+					render: function(){
+						console.log('rendering');
+						// add the page elements to the DOM for temporary displaying
+						// clear the container and start fresh
+						$('#pagebuilder div').empty();
+						// add header
+						$('#pagebuilder div').append('<header><img src="icons/ws/'+this.img+'"><h1>'+this.name+'</h1></header>');
+						if(this.temperature_item != ''){
+							$('#pagebuilder div header').append('<div class="value"><span data-role="displayvalue" data-item="'+this.temperature_item+'" data-digits="1"></span>&deg;C</div>');
+						}
+						
+						// sections
+						$.each(this.section,function(index,section){
+							if(section.type=='collabsible'){
+								$('#pagebuilder div').append('<section data-role="collapsible" data-theme="a" data-collapsed="false" data-id="'+index+'"><h1>'+section.name+'</h1>');
+							}
+							else if(section.type=='collabsible'){
+								$('#pagebuilder div').append('<section data-role="collapsible" data-theme="a" data-collapsed="true" data-id="'+index+'"><h1>'+section.name+'</h1>');
+							}
+							else{
+								$('#pagebuilder div').append('<section data-id="'+index+'">');
+							}
+							// widgets
+							$.each(section.widget,function(index,widget){
+								$('#pagebuilder div').append('<div data-role="'+widget+'" '+options+' data-id="'+index+'"></div>');
+							});
+							$('#pagebuilder div').append('</section>');
+						});
+						
+						// enhance
+						$('#pagebuilder').enhanceWithin();
 					}
 				});
 			},
@@ -51,12 +98,23 @@ pagebuilder = {
 				delete this;
 			}
 		});
-	},
+	}
 };
 
 
+// test
+pagebuilder.add_section();
+pagebuilder.section[0].update('firstfloor','First floor');
+pagebuilder.section[0].add_page();
+pagebuilder.section[0].page[0].update('living','Living','scene_livingroom.png','living.measurements.temperature');
+console.log(JSON.stringify(pagebuilder));
+
+$(document).on('pageinit','#pagebuilder',function(){
+	pagebuilder.section[0].page[0].render();
+});
+
 /* example model
-model = {
+pagebuilder = {
 	home: {
 		name: 'Home',
 		section: [
