1fd5ac60c892f21c6b26075bfb4852ad9009d4be
BrechtBa <brecht.baeten@gmail.com>
2015-04-13 21:12:03 +0200

    updated weather predictions

--- smarthome/logics/predictions_weather.py
+++ smarthome/logics/predictions_weather.py
@@ -6,6 +6,7 @@
 import urllib.request
 import json
 
+# detailed predictions
 weatherforecast = []
 
 response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast?lat=%s&lon=%s' % (sh._lat,sh._lon))
@@ -27,5 +28,34 @@ for forecast in response['list']:
 		
 	weatherforecast.append(currentforecast)
 
-sh.building.weatherforecast(weatherforecast)
-logger.warning('Weather forecast loaded')
+sh.knxcontrol.weather.predictions.detailed(weatherforecast)
+
+
+# daily predictions
+weatherforecast = []
+
+response = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/forecast/daily?lat=%s&lon=%s' % (sh._lat,sh._lon))
+response = json.loads(response.read().decode('utf-8'))
+
+for forecast in response['list']:
+	currentforecast = {'datetime': forecast['dt']}
+	currentforecast['temperature_day'] = forecast['temp']['day']-273.15
+	currentforecast['temperature_night'] = forecast['temp']['night']-273.15
+	currentforecast['pressure'] = forecast['pressure']
+	currentforecast['humidity'] = forecast['humidity']
+	currentforecast['icon'] = forecast['weather'][0]['icon']
+	currentforecast['cloudfactor'] = forecast['clouds']
+	currentforecast['wind_speed'] = forecast['speed']
+	currentforecast['wind_directions'] = forecast['deg']
+	if 'rain' in forecast:
+		currentforecast['rain'] = forecast['rain']
+	else:
+		currentforecast['rain'] = 0
+		
+	weatherforecast.append(currentforecast)
+
+sh.knxcontrol.weather.predictions.daily(weatherforecast)
+
+
+
+logger.info('Weather forecast loaded')
