9a1f08df82006d31389d13bcf497e526d6c994df
brecht <brecht.baeten@gmail.com>
2015-04-25 18:30:18 +0200

    added parsing for some contraints

--- plugins/knxcontrol/mpc.py
+++ plugins/knxcontrol/mpc.py
@@ -32,23 +32,29 @@ class OCmodel:
 		# create variables
 		self.nvars = 0
 		self.vars = {}
-		self.vars['T_zon'] = _op_variable(self._sh,self,False,items=['knxcontrol.building.living_zone.temperature'],operation='avg')
-		self.vars['T_amb'] = _op_variable(self._sh,self,False,items=['knxcontrol.weather.current.temperature'],operation='avg')
-		self.vars['P_emi'] = _op_variable(self._sh,self,False,items=['knxcontrol.building.*.emission.power'],operation='sum')
-		self.vars['P_sol'] = _op_variable(self._sh,self,False,items=['knxcontrol.building.*.irradiation.power'],operation='sum')
-		self.vars['P_pro'] = _op_variable(self._sh,self,False,items=['knxcontrol.heat_production.*.power'],operation='sum')
-		self.vars['V_ven'] = _op_variable(self._sh,self,False,items=['knxcontrol.ventilation.*.fanspeed'],operation='sum')
-		self.vars['C_zon'] = _op_variable(self._sh,self,True)		
-		self.vars['UA_zon_amb'] = _op_variable(self._sh,self,True)
-		self.vars['n_emi'] = _op_variable(self._sh,self,True)
-		self.vars['n_sol'] = _op_variable(self._sh,self,True)
-		self.vars['n_pro'] = _op_variable(self._sh,self,True)
-		self.vars['n_ven'] = _op_variable(self._sh,self,True)
+		self.vars['T_zon'] = _op_variable(self,False,items=['knxcontrol.building.living_zone.temperature'],operation='avg')
+		self.vars['T_amb'] = _op_variable(self,False,items=['knxcontrol.weather.current.temperature'],operation='avg')
+		self.vars['P_emi'] = _op_variable(self,False,items=['knxcontrol.building.*.emission.power'],operation='sum')
+		self.vars['P_sol'] = _op_variable(self,False,items=['knxcontrol.building.*.irradiation.power'],operation='sum')
+		self.vars['P_pro'] = _op_variable(self,False,items=['knxcontrol.heat_production.*.power'],operation='sum')
+		self.vars['V_ven'] = _op_variable(self,False,items=['knxcontrol.ventilation.*.fanspeed'],operation='sum')
+		self.vars['C_zon'] = _op_variable(self,True)		
+		self.vars['UA_zon_amb'] = _op_variable(self,True)
+		self.vars['n_emi'] = _op_variable(self,True)
+		self.vars['n_sol'] = _op_variable(self,True)
+		self.vars['n_pro'] = _op_variable(self,True)
+		self.vars['n_ven'] = _op_variable(self,True)
 		
 
-		logger.warning(self.vars['T_zon'].measurement)
-		
+		logger.warning(self.vars['P_sol'].measurement)
+		logger.warning(self.vars['P_sol'].ind)
 
+		# create constraints
+		self.cons['T_zon_state'] = _op_constraint(self,'C_zon*(T_zon[i+1]-T_zon[i])/dt - UA_zon_amb*(T_amb[i]-T_zon[i]) - n_emi*P_emi[i] - n_sol*P_sol[i]',0,0,
+                                                      {'C_zon':'(T_zon[i+1]-T_zon[i])/dt','T_zon[i+1]':'C_zon/dt','T_zon[i]':'-C_zon/dt+UA_zon_amb','UA_zon,amb':'-(T_amb[i]-T_zon[i])','T_amb[i]':'-UA_zon_amb','n_emi':'-P_emi[i]','P_emi[i]':'-n_emi','n_sol':'-P_sol[i]','P_sol[i]':'-n_sol'},
+ 													  {'C_zon*T_zon[i+1]':'1/dt','C_zon*T_zon[i]':'-1/dt','UA_zon_amb*T_amb[i]':'-1','UA_zon_amb*T_zon[i]':'-1','n_emi*P_emi[i]':'-1','n_sol*P_sol[i]':'-1'},i=np.arange(self.N))
+
+		logger.warning(self.cons['T_zon_state'].c)
 		#self.identify()
 
 
@@ -60,25 +66,23 @@ class OCmodel:
 	
 class _op_variable:
 
-	def __init__(self,smarthome,ocmodel,parameter,items=None,operation=None):
+	def __init__(self,ocmodel,parameter,items=None,operation=None):
 		"""
 		Defines a variable in the optimization problems
 		Inputs:
-		smarthome:  the smarthome object
 		ocmodel:    the parent optimal control model
 		parameter:  true/false if it is a parameter (fixed over time)
 		items:      list of itemstrings for the measurement item and
         avg:        a boolean if an average or total is to be taken
 		"""
-		self._sh = smarthome
 		self._ocmodel = ocmodel
 
 		if parameter:
-			self.ind = self._ocmodel.nvars
+			self.ind = [self._ocmodel.nvars]
 			self._ocmodel.nvars = self._ocmodel.nvars + 1
 		else:
-			self.ind = self._ocmodel.nvars + np.arange(len(self._ocmodel.t))
-			self._ocmodel.nvars = self._ocmodel.nvars + np.arange(len(self._ocmodel.t))
+			self.ind = self._ocmodel.nvars + np.arange(self._ocmodel.N+1)
+			self._ocmodel.nvars = self._ocmodel.nvars + self._ocmodel.N + 1
 		
 		values = np.empty((self._ocmodel.N+1,0))
 
@@ -89,13 +93,15 @@ class _op_variable:
 				if np.any(value):
 					values = np.concatenate((values,value),axis=1)
 
-		# average or total the data if required
-		if operation == 'avg':
-			self.measurement = np.mean(values,axis=1)
-		elif operation == 'sum':
-			self.measurement = np.sum(values,axis=1)
+			# average or total the data if required
+			if operation == 'avg':
+				self.measurement = np.mean(values,axis=1)
+			elif operation == 'sum':
+				self.measurement = np.sum(values,axis=1)
+			else:
+				self.measurement = values
 		else:
-			self.measurement = values
+			self.measurement = None
 
 		self.value = None
 		
@@ -110,7 +116,7 @@ class _op_variable:
 
 		ids = []
 		try:
-			for item in self._sh.match_items(itemstr):
+			for item in self._ocmodel._sh.match_items(itemstr):
 				ids.append(item.conf['mysql_id'])
 		except:
 			logger.warning('Could not find mysql_id for %s' % (itemstr))
@@ -118,7 +124,7 @@ class _op_variable:
 
 		value = np.empty((self._ocmodel.N+1,0))
 
-		con = pymysql.connect('localhost', 'knxcontrol', self._sh.knxcontrol.mysql.conf['password'], 'knxcontrol')
+		con = pymysql.connect('localhost', 'knxcontrol', self._ocmodel._sh.knxcontrol.mysql.conf['password'], 'knxcontrol')
 		cur = con.cursor()
 
 		for ind,id in enumerate(ids):
@@ -138,18 +144,52 @@ class _op_variable:
 			
 
 class _op_constraint:
-	def __init__(self,smarthome,ocmodel,parameter,items=None,operation=None):
-	"""
-	Defines a constraint in the optimization problems
-	Inputs:
-	smarthome:  the smarthome object
-	ocmodel:    the parent optimal control model
-	parameter:  true/false if it is a parameter (fixed over time)
-	items:      list of itemstrings for the measurement item and
-    avg:        a boolean if an average or total is to be taken
-	"""
-	self._sh = smarthome
-	self._ocmodel = ocmodel
+
+	def __init__(self,ocmodel,constraint,constraint_min,constraint_max,jacobian,hessian,i=0):
+		"""
+		Defines a constraint in the optimization problems of the form c_min <= c <= c_max
+	
+		Inputs:
+		ocmodel:          the parent optimal control model
+		constraint:       an expression for the value of the constraint function 
+		constraint_min:   a minimum value or array of minimum values for the constraint
+		constraint_max:   a maximum value or array of maximum values for the constraint
+		jacobian:         a dict defining the nonzero 1st derivatives of the constraint with expressions e.g. {'x':'dc/dx'}
+		hessian:          a dict defining the nonzero 2nd derivatives of the constraint with expressions e.g. {'x*y':'d2c/dx/dy'}
+		i                 an array of indices len(i) constriants will be created,'[i-1]','[i]' and '[i+1]' will be replaced by its value
+		"""
+
+		self._ocmodel = ocmodel
+		
+		self.c_min = constraint_min
+		self.c_max = constraint_max
+	
+		self.c = []
+		self.J = []
+		for ind in i:
+			# parse the constraint string
+			temp = constraint
+			for v in self._ocmodel.vars:
+				if ind   >= 0 and ind   < len(self._ocmodel.vars[v].ind):
+					temp = temp.replace(v+'[i]', 'x[%s]' % (self._ocmodel.vars[v].ind[ind]))
+				if ind+1 >= 0 and ind+1 < len(self._ocmodel.vars[v].ind):
+					temp = temp.replace(v+'[i+1]', 'x[%s]' % (self._ocmodel.vars[v].ind[ind+1]))
+				if ind-1 >= 0 and ind-1 < len(self._ocmodel.vars[v].ind):
+					temp = temp.replace(v+'[i-1]', 'x[%s]' % (self._ocmodel.vars[v].ind[ind-1]))
+
+				temp = temp.replace(v, 'x[%s]' % (self._ocmodel.vars[v].ind[0]))
+
+				temp = temp.replace('dt', '%s' % (self._ocmodel.dt))
+			self.c.append(temp)
+
+
+			# parse jacobian string
+			#self.J.append( '[0' + ',0'*(self._ocmodel.nvars-1) + ']' )
+			
+
+		logger.warning(self.c)
+
+
 
 
 
