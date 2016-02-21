3ee9fec2ea87cae02e0504a34aa3fece7bd55e8d
brecht <brecht.baeten@gmail.com>
2016-02-21 16:44:04 +0100

    started polymer development

---
+++ www/scripts/app.js
@@ -0,0 +1,97 @@
+
+(function(document) {
+	'use strict';
+
+	// Grab a reference to our auto-binding template
+	// and give it some initial binding values
+	// Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+	var app = document.querySelector('#app');
+
+	// Sets app default base URL
+	app.baseUrl = '';
+	if (window.location.port === '') {  // if production
+		// Uncomment app.baseURL below and
+		// set app.baseURL to '/your-pathname/' if running from folder in production
+		// app.baseUrl = '/polymer-starter-kit/';
+	}
+	app.displayInstalledToast = function() {
+		// Check to make sure caching is actually enabled—it won't be in the dev environment.
+		if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
+			Polymer.dom(document).querySelector('#caching-complete').show();
+		}
+	};
+
+	app.checkToken = function(token){
+		// Checks if the app token is valid
+		var valid = false;
+		try{
+			var parts = token.split('.');
+			var header = JSON.parse(atob(parts[0]));
+			var payload = JSON.parse(atob(parts[1]));
+
+			if( Date.now() / 1000 < payload['exp'] ){
+				valid = true;
+			}
+		}
+		catch(e){
+		}
+		
+		valid = true; // for testing
+		return valid;
+
+	};
+	app.checkRoute = function(route,token){
+		var allowed = false;
+		// check if route is valid
+		if(app.checkToken(token)){
+
+			var parts = app.token.split('.');
+			var header = JSON.parse(atob(parts[0]));
+			var payload = JSON.parse(atob(parts[1]));
+
+			try{
+				if(payload['permission']>=9){
+					allowed =  true;
+				}
+				else if(payload['permission']>=1){
+					if( ['login','ranking','pron','results','profile','logout'].indexOf(route)>-1 ){
+						allowed = true
+					}
+				}
+				else{
+					if( ['login','logout'].indexOf(route)>-1 ){
+						allowed = true
+					}
+				}
+			}
+			catch(e){
+			}
+		}
+		allowed = true;  // for testing
+		return allowed;
+	}
+
+	// Listen for template bound event to know when bindings
+	// have resolved and content has been stamped to the page
+	app.addEventListener('dom-change', function() {
+		console.log('Our app is ready to rock!');
+	});
+
+
+	// See https://github.com/Polymer/polymer/issues/1381
+	window.addEventListener('WebComponentsReady', function() {
+		// imports are loaded and elements have been registered
+	});
+
+	// Scroll page to top and expand header
+	app.scrollPageToTop = function() {
+		//app.$.headerPanel.scrollToTop(true);
+	};
+
+	app.closeDrawer = function() {
+		app.$.main.closeDrawer();
+	};
+
+
+
+})(document);
